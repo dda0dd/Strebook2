e04999d3fb3bdea383db2ab7f76e53e6dfdb9edb
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-09-08 01:59:59 +0000

    [Break] 会員機能記述スタート

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,61 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @customers = Customer.all.page(params[:page]).per(5)
   end
 
   def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+    @book_store = Book_store.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: "会員情報を更新しました。"
+    else
+      render :edit
+    end
+    @book_store = Book_store.find(params[:id])
+    if @book_store.update(book_store_params)
+      redirect_to admin_book_store_path(@book_store), notice: "会員情報を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+    @customer = Customer.find(params[:id])
+  end
+
+  def withdraw
+    customer = Customer.find(params[:id])
+    customer.update(is_active: false)
+    redirect_to admin_customers_path
+  end
+
+  # def search
+  #   @word = params[:is_active]
+  #   if @word.present?
+  #     @customers = Customer.where(is_active: @word).page(params[:page]).per(5)
+  #   else
+  #     @customers = Customer.all.page(params[:page]).per(5)
+  #   end
+  #   render "admin/customers/index"
+  # end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :age, :address, :occupation, :is_active)
+  end
+
+  def book_store_params
+    params.require(:book_store).permit(:name, :age, :address, :telephone_number, :is_active)
   end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,51 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :ensure_guest_customer, only: [:edit]
+
   def edit
+    @customer = current_customer
   end
 
   def show
+    @customer = current_customer
+    @request_comments = @customer.request_comments.page(params[:page]).per(5)
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "success"
+      redirect_to public_customer_path(params[:id])
+    else
+      flash.now[:alert] = "failer"
+      render :edit
+    end
   end
 
   def unsubscribe
+  end
+
+  def withdraw
+    current_customer.update(is_active: false)
+    reset_session
+    redirect_to new_customer_registration_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name,
+                                     :age,
+                                     :address,
+                                     :occupation,
+                                     :email
+                                     )
+  end
+
+  def ensure_guest_customer
+    @customer = Customer.find(params[:id])
+    if @customer.guest?
+      redirect_to customer_path(current_customer) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
   end
 end
