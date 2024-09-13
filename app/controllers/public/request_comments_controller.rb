279a35c48f8013c4554e9651b4c3bacb62301cdd
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-09-13 00:49:12 +0000

    [Finish] お客様のコメント機能記述完了

--- app/controllers/public/request_comments_controller.rb
+++ app/controllers/public/request_comments_controller.rb
@@ -1,4 +1,23 @@
 class Public::RequestCommentsController < ApplicationController
   def new
+    @request_comment = RequestComment.new
+  end
+
+  def create
+    @request_comment = current_customer.request_comments.new(request_comment_params)
+    @request_comment.save!
+    redirect_to public_customer_path(current_customer)
+  end
+
+  def destroy
+    @request_comment = RequestComment.find(params[:id])
+    @request_comment.destroy
+    redirect_to public_customer_path(params[:id])
+  end
+
+  private
+
+  def request_comment_params
+    params.require(:request_comment).permit(:content)
   end
 end
