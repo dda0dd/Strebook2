3a7049c024d2b2edc033102f18775ce43e7548da
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-09-14 02:52:28 +0000

    [Finish] 管理者・書店のレビューとコメント検索機能記述完了

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,4 +1,17 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @thoughtse_comments = Post.find(params[:id]).thoughtse_comments.page(params[:page]).per(5)
+  end
+
+  def destroy
+    @thoughtse_comment = ThoughtseComment.find(params[:id])
+    @thoughtse_comment.destroy
+    redirect_to admin_comments_path(id: @thoughtse_comment.post_id)
+  end
+
+  def search
+    @word = params[:word]
+    @thoughtse_comments = ThoughtseComment.where("content LIKE?", "%#{@word}%").page(params[:page]).per(5)
+    render "admin/comments/index"
   end
 end
--- app/controllers/admin/request_comments_controller.rb
+++ app/controllers/admin/request_comments_controller.rb
@@ -1,4 +1,17 @@
 class Admin::RequestCommentsController < ApplicationController
   def index
+    @request_comments = RequestComment.all.page(params[:page]).per(5)
+  end
+
+  def destroy
+    @request_comment = RequestComment.find(params[:id])
+    @request_comment.destroy
+    redirect_to admin_request_comments_path
+  end
+
+  def search
+    @word = params[:word]
+    @request_comments = RequestComment.where("content LIKE?", "%#{@word}%").page(params[:page]).per(5)
+    render "admin/request_comments/index"
   end
 end
